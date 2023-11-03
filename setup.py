53885241d4f7ad73466b7dd27c67629d5ce3cd40
@Saidi-Souhaieb <Saidisouhaieb@takiacademyteam.com>
2023-11-03 06:57:14 +0100

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dynamicscrapper',
-    version='0.1.0',
+    version='0.2.0',
     author='Saidi Souhaieb',
     author_email='Saidisouhaieb@takiacademyteam.com',
     description='A package to scrap the web dynamically',
