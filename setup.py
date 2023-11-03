6957f7619b4620e186494ef45f26abd99e75b674
@Saidi-Souhaieb <Saidisouhaieb@takiacademyteam.com>
2023-11-03 07:44:57 +0100

    added opening file readme

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
     name='dynamicscrapper',
     version='0.3.0',
     author='Saidi Souhaieb',
     author_email='Saidisouhaieb@takiacademyteam.com',
     description='A package to scrap the web dynamically',
-    long_description="""# Markdown supported!\n\n* Cheer\n* Celebrate\n""",
-    long_description_content_type='text/markdown',
-    readme="README.md",
+
+    long_description=readme.md,
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
