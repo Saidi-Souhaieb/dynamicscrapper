657288b60795d48a077cff69a71ab3bf112cf718
@Saidi-Souhaieb <Saidisouhaieb@takiacademyteam.com>
2023-11-03 07:41:15 +0100

    added pypi description

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='dynamicscrapper',
-    version='0.2.0',
+    version='0.3.0',
     author='Saidi Souhaieb',
     author_email='Saidisouhaieb@takiacademyteam.com',
     description='A package to scrap the web dynamically',
+    long_description="""# Markdown supported!\n\n* Cheer\n* Celebrate\n""",
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
