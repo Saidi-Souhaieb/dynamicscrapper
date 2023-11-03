3fd0f39046f062b80005d11da712175637de8d3f
@Saidi-Souhaieb <Saidisouhaieb@takiacademyteam.com>
2023-11-03 10:31:18 +0100

    fix the package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='dynamicscrapper',
-    version='5.4.1',
+    version='5.4.2',
     author='Saidi Souhaieb',
     author_email='Saidisouhaieb@takiacademyteam.com',
     description='A package to scrap the web dynamically',
